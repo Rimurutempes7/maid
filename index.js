1911c5e60e03e512e901320d99ec7b85770053c7
Rimurutempes7 <84686146+Rimurutempes7@users.noreply.github.com>
2023-05-22 18:33:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -799,3 +799,28 @@ async function starts() {
 	})
 }
 starts()
+
+              
+
+
+javascript
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Cliente pronto!');
+});
+
+client.initialize();
+
+
+
+
+
+
